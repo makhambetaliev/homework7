b3d46e10d35cf4958a122099addd4f6e4b04a498
makhambetaliev <r.maxambetaliev@newuu.uz>
2024-11-22 12:38:41 +0500

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void years2days(int &days, int &years) {
+    if (days>=365) {
+        years=days/365;
+        days=days%365;
+    }
+    else {
+        years = 0;
+        days = days;
+    }
+}
+int main() {
+    int days, years;
+    cin>>days;
+    years2days(days, years);
+    cout<<years<<" "<<days<<endl;
+}
